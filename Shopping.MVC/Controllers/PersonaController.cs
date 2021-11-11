3cae679e274200fd12bdd91faa39fb59500929ed
Leonardo2506 <maggical2014.lf@gmail.com>
2021-11-11 17:46:36 -0300

    UpdateHTML

--- Shopping.MVC/Controllers/PersonaController.cs
+++ Shopping.MVC/Controllers/PersonaController.cs
@@ -27,7 +27,7 @@ namespace Shopping.MVC.Controllers
         public IActionResult DarAltaPersona(Persona persona) 
         {
             Repositorio.AgregarPersona(persona);
-            return View("El local esta a cargo por", persona);
+            return View("El local esta a cargo por ", Repositorio.Personas);
         }
 
         
